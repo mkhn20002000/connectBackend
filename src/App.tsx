5638eec4ffba4e2efff07e8f95cf65581f876111
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-11-24 10:01:57 +0100

    without services

---
+++ src/App.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios, { CanceledError } from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((item) => item.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Manoochehr Khatami" };
+    setUsers([newUser, ...users]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then((res) => setUsers([res.data, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user:User) => {
+    const originalUsers = [...users];
+    const updatedUser = {...user, name: user.name + '!'}
+    setUsers(users.map(u=> u.id === user.id ? updatedUser : u));
+
+    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+    .catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  }
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            {user.name}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                UPDATE
+              </button>
+              <button
+                onClick={() => deleteUser(user)}
+                className="btn btn-outline-danger"
+              >
+                DELETE
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
+// 'https://jsonplaceholder.typicode.com/users'
---
+++ src/ProductList.tsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react'
+const ProductList = ({category}:{category: string}) => {
+    const [products, setProducts] = useState<string[]>([])
+
+    useEffect(()=>{
+        console.log('Fetching products in ', category);
+        setProducts(['Clothing','Household'])
+        
+    },[category])
+  return (
+    <div>
+      ProductList
+    </div>
+  )
+}
+
+export default ProductList
